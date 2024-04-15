28b085a3b1261f9a125161e7e8d5159dd6c24ef2
dahmana <dhabooode@gmail.com>
2024-04-15 10:59:34 -0700

    added comments

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // Creating the string for the user to see
 notification = ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
 
-// Assigning variables equal to 10 - 40 - 39
+// Assigning variables equal to 10 - 40 - 39:
 
+// a equals to 10 using addition
 let a = 5+5
+
+// b equals to 40 using multiplication
 let b = 20*2
+
+// c equals to 39 using subtraction
 let c = 40-1
 
 alert(notification + "\n " + "\n" + a + " " + "-" + " " + b + " " + "-" + " "+ c);

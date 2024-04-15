9f30a5556fd5ebaf9f0d88818f3f2ad926b65540
dahmana <dhabooode@gmail.com>
2024-04-15 10:42:03 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Creating the string for the user to see
+notification = ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+// Assigning variables equal to 10 - 40 - 39
+
+let a = 5+5
+let b = 20*2
+let c = 40-1
+
+alert(notification + "\n " + "\n" + a + " " + "-" + " " + b + " " + "-" + " "+ c);
